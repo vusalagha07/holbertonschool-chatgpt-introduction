3c2b43a7a69558004f2401092eafa7453bd2c605
vusalagha07 <13877@holbertonstudents.com>
2026-05-15 07:57:57 +0000

    Add fixed factorial.py

---
+++ debugging/factorial.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import sys
+
+def factorial(n):
+    result = 1
+    while n > 1:
+        result *= n
+        n -= 1
+    return result
+
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        f = factorial(int(sys.argv[1]))
+        print(f)
